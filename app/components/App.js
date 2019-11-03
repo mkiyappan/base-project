a5b72fed3a1596594f81fb19777a8bec820e790f
Iyappan <iyappanmk91@gmail.com>
2019-11-03 20:56:50 +0530

    based react project with material ui

---
+++ app/components/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import Button from '@material-ui/core/Button';
+
+class App extends Component {
+
+    render(){
+        return (
+            <div>
+                Image Upload -1
+                <Button variant="contained" color="primary">
+                    Hello World
+                </Button>
+            </div>
+        );
+    }
+}
+export default App;
